e24d2951bbe158a2cdca77a5b69e751a763d3cb1
Konstantin Tsistjakov <ktsistjakov@gmail.com>
2017-01-26 11:15:28 +0200

    Initial Commit

---
+++ SplitController/SplitController.h
@@ -0,0 +1,19 @@
+//
+//  SplitController.h
+//  SplitController
+//
+//  Created by Konstantin Tsistjakov on 26/01/2017.
+//  Copyright © 2017 Chipp Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SplitController.
+FOUNDATION_EXPORT double SplitControllerVersionNumber;
+
+//! Project version string for SplitController.
+FOUNDATION_EXPORT const unsigned char SplitControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SplitController/PublicHeader.h>
+
+
