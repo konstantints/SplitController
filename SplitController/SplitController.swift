c08620823d06b78378db1e61b0d6ef81c934af81
Konstantin Tsistjakov <ktsistjakov@gmail.com>
2017-01-26 11:22:44 +0200

    This is where it all begins...

---
+++ SplitController/SplitController.swift
@@ -0,0 +1,24 @@
+//
+//  SplitController.swift
+//  SplitController
+//
+//  Created by Konstantin Tsistjakov on 26/01/2017.
+//  Copyright © 2017 Chipp Studio. All rights reserved.
+//
+
+import UIKit
+
+class SplitController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
